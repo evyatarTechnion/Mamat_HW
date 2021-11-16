62a59f2ea222698f1a8c4597eaaf83618bf6b238
1elkana1 <1elkana1@gmail.com>
2021-11-16 17:50:08 +0200

    calc_statistics.c uploaded

---
+++ HW_2/calc_statistics.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/* Integer constants */
+enum { MAX_GRADE=100, PASS_GRADE=55, MIN_GRADE=1, CENTUM=100, TEN=10 };
+
+/**
+ * @brief Application's main entry point
+ * @param argc Number of arguments
+ * @param argv Pointer to arguments
+ * @return Non-zero value in case of an error
+ */
+int main(int argc, char *argv[]) {
+    if (argc < 1) {
+        fprintf(stderr, "Error\n");
+        return 1;
+    }
+
+	/* import files to read/write */
+	FILE *grades, *stats;
+	grades = fopen("grades.txt", "r");
+	stats = fopen("course_statistics.txt", "w");
+
+	int count_grades[MAX_GRADE] = {0};
+	int count_students = 0;
+	int grades_sum = 0;
+	int max_grade = MIN_GRADE, min_grade = MAX_GRADE;
+	int passed = 0;
+
+	/* scan grades and extract data */
+	int cur_grade = 0;
+	while (fscanf(grades, "%d\n", &cur_grade) != EOF) {
+		count_students++;
+		count_grades[cur_grade-1]++;
+		grades_sum += cur_grade;
+		if (cur_grade >= PASS_GRADE) {
+			passed++;
+		}
+		if (cur_grade > max_grade) {
+			max_grade = cur_grade;
+		}
+		if (cur_grade <= min_grade) {
+			min_grade = cur_grade;
+		}
+	}
+
+	/* calculate average grade and pass rate */
+	double avg = (double)grades_sum/(double)count_students;
+	double pass_rate  = ((double)passed/(double)count_students)*CENTUM;
+
+	/* calculate median grade */
+	int median_position = (count_students+1)/2;
+	int count_low_grade_students = 0;
+	cur_grade = MIN_GRADE;
+	while (count_low_grade_students < median_position) {
+		count_low_grade_students += count_grades[cur_grade-1];
+		cur_grade++;
+	}
+	int median = cur_grade;
+
+	/* print statistics */
+	fprintf(stats, "num of students = %d\n", count_students);
+	fprintf(stats, "avg = %.3f\n", avg);
+	fprintf(stats, "the median is - %d\n", median);
+	fprintf(stats, "max grade = %d, min grade = %d\n", max_grade, min_grade);
+	fprintf(stats, "pass rate = %.2f%%\n", pass_rate);
+
+	/* print grades histogram */
+	cur_grade = MIN_GRADE;
+	for (int i = 0; i < TEN; i++) {
+		for (int j = 0; j < TEN-1; j++) {
+			printf(stats, "%d ",count_grades[cur_grade-1]);
+			cur_grade++;
+		}
+		printf(stats, "%d\n",count_grades[cur_grade-1]);
+		cur_grade++;
+	}
+
+	/* close all files */
+	fclose(stats);
+	fclose(grades);
+
+	return 0;
+}
