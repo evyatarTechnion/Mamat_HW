4f9b3ead77af20bb1e510582cc5f53ca7e66edba
evyatarTechnion <evyatarnahum@campus.technion.ac.il>
2021-11-24 01:19:06 +0200

    fix syntax

--- HW1/scrape_players.sh
+++ HW1/scrape_players.sh
@@ -4,18 +4,16 @@
 wget https://www.premierleague.com/players
 
 # search for all players link, all links will be in urls.txt file
-while read line; do
-	for word in $line; do
-		echo $word | grep -oP '(\/players\/)([0-9])+(\/)([a-zA-Z-])+(\/overview)' >> urls.txt
-	done
-done <$'players'
+grep -oP '(\/players\/)([0-9])+(\/)([a-zA-Z-])+(\/overview)' players > urls.txt
+
 
 # update all links found to currect format - into update_urls.txt file
-sed  's/\/players/https:\/\/www\.premierleague\.com\/players/' $'urls.txt' > update_urls.txt
+sed -i 's/\/players/https:\/\/www\.premierleague\.com\/players/' urls.txt
 
-# initialize the uouput file with total count of players fount - player_results.csv
-tot=`cat $'update_urls.txt' | wc -l`
-echo "Total Players: $tot" > player_results.csv
+# initialize the output file with total count of players fount - 
+# player_results.csv
+tot=`cat urls.txt | wc -l`
+echo "Total players: $tot" > player_results.csv
 
 # search in all players links for midfielder keyword.
 while read line; do
@@ -33,4 +31,7 @@ while read line; do
 		echo "$line, midfielder, $counter" >> player_results.csv
 	fi
 	rm overview
-done <$'update_urls.txt'
+done <$'urls.txt'
+
+rm players
+rm urls.txt
